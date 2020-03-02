be7e607a9adfeb214cb20012fb15b45c6a42348e
Abin Simon <abinsimon10@gmail.com>
2020-03-02 15:09:10 +0530

    bump ytdd

--- Formula/ytdd.rb
+++ Formula/ytdd.rb
@@ -1,9 +1,9 @@
 class Ytdd < Formula
-  desc "Download all the youtube vids that you copy tot clipboard"
+  desc "Download all the youtube vids that you copy to clipboard"
   homepage "https://github.com/meain/ytdd"
-  url "https://github.com/meain/ytdd/releases/download/v0.3/ytdd_0.3_darwin_amd64.tar.gz"
-  version "0.0.3"
-  sha256 "12d7039436531364fbc4d2671826fdd2b8e53bbc70f0ee584aee615f9007c5ad"
+  url "https://github.com/meain/ytdd/releases/download/v0.4/ytdd_0.4_darwin_amd64.tar.gz"
+  version "0.0.4"
+  sha256 "0e1a5640411b7ef21db5333999cbf6bf5648743890af5068ab351cdfab95a43e"
 
   def install
     bin.install "ytdd"
