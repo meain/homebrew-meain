29c7ad18115533b16614fc3cea12ea00886c188f
Abin Simon <abinsimon10@gmail.com>
2020-05-27 12:12:27 +0530

    woops

--- Formula/tojson.rb
+++ Formula/tojson.rb
@@ -2,11 +2,11 @@ class Tojson < Formula
   desc "Convert from yaml and toml to json"
   homepage "https://github.com/meain/tojson"
   url "https://github.com/meain/tojson/releases/download/0.1.1/tojson-macos-amd64"
-  version "0.1.2"
+  version "0.1.3"
   sha256 "d91c890995618e9862d7eca91275c95ce612aba0c570df6093bf89c8d8b4fb9a"
 
   def install
-    mv Dir.glob("tojson-*").first, "foo"
+    mv Dir.glob("tojson-*").first, "tojson"
     bin.install "tojson"
   end
 
