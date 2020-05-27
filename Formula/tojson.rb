572668fb7b5c6f3cfbe35997b7068e7b67269e80
Abin Simon <abinsimon10@gmail.com>
2020-05-27 12:52:18 +0530

    bump tojson

--- Formula/tojson.rb
+++ Formula/tojson.rb
@@ -1,9 +1,9 @@
 class Tojson < Formula
   desc "Convert from yaml and toml to json"
   homepage "https://github.com/meain/tojson"
-  url "https://github.com/meain/tojson/releases/download/0.1.1/tojson-macos-amd64"
-  version "0.1.3"
-  sha256 "d91c890995618e9862d7eca91275c95ce612aba0c570df6093bf89c8d8b4fb9a"
+  url "https://github.com/meain/tojson/releases/download/0.1.5/tojson-macos-amd64"
+  version "0.1.5"
+  sha256 "d7b5654c4156358640947a0c908342c0ad31662eea30581716b7ac4c63c0a8b5"
 
   def install
     mv Dir.glob("tojson-*").first, "tojson"
