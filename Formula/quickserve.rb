5a590d93f7f24784ae1bcb666916b5936630f351
meain <meain@users.noreply.github.com>
2021-09-10 03:13:13 +0000

    update brews

--- Formula/quickserve.rb
+++ Formula/quickserve.rb
@@ -1,9 +1,9 @@
 class Quickserve < Formula
   desc "Quickly serve a dir"
   homepage "https://github.com/meain/quickserve"
-  url "https://github.com/meain/quickserve/releases/download/0.4.0/quickserve-macos-amd64"
-  version "0.4.0"
-  sha256 "66349845436117db0b623e00284367e0ecec10a5943531150ce1a44b9e900a0b"
+  url "https://github.com/meain/quickserve/releases/download/0.5.0/quickserve-macos-amd64"
+  version "0.5.0"
+  sha256 "cd740275815dc5df905a036c5512e95b035f65637aa5cb5f2847669e3ced674a"
 
   def install
     mv Dir.glob("quickserve-*").first, "quickserve"
