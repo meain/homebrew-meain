f604bf568db4c0e3c7a347ebe31f5e88f739af02
meain <meain@users.noreply.github.com>
2021-09-15 03:13:28 +0000

    update brews

--- Formula/rest.rb
+++ Formula/rest.rb
@@ -1,9 +1,9 @@
 class Rest < Formula
   desc "Send http request with spec from file"
   homepage "https://github.com/meain/rest"
-  url "https://github.com/meain/rest/releases/download/0.1.0/rest_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "5a41c9b427422eb69a0359890abba4871d7d892b74cd0b802f9b0a50fc08eaae"
+  url "https://github.com/meain/rest/releases/download/0.2.0/rest_0.2.0_darwin_amd64.tar.gz"
+  version "0.2.0"
+  sha256 "019bbf8ee745bf363b9ad47788a94692d854d40198f941decd591e692e22e4b0"
 
   def install
     bin.install "rest"
