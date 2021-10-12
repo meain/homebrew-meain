a8d458f771d25007f9e190cae070d6850771c7fa
meain <meain@users.noreply.github.com>
2021-10-12 03:13:42 +0000

    update brews

--- Formula/gh-issues-to-rss.rb
+++ Formula/gh-issues-to-rss.rb
@@ -1,9 +1,9 @@
 class GhIssuesToRss < Formula
   desc "Convert github issues and prs into rss feed "
   homepage "https://github.com/meain/gh-issues-to-rss"
-  url "https://github.com/meain/gh-issues-to-rss/releases/download/0.3.0/gh-issues-to-rss_0.3.0_darwin_amd64.tar.gz"
-  version "0.3.0"
-  sha256 "fa04360582c49e592adec1845110264c2a22322bd33faf3c54fde1a7ab56b5e2"
+  url "https://github.com/meain/gh-issues-to-rss/releases/download/0.3.2/gh-issues-to-rss_0.3.2_darwin_amd64.tar.gz"
+  version "0.3.2"
+  sha256 "f1ee35a72525ac566f9178d97dde276614e92e0baab156bca5f82976267ea71e"
 
   def install
     bin.install "gh-issues-to-rss"
