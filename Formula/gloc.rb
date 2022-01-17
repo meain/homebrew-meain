cc07198c2a1d6e64bb5fe9e5d01559824fe9f3a2
meain <meain@users.noreply.github.com>
2022-01-17 03:23:45 +0000

    update brews

--- Formula/gloc.rb
+++ Formula/gloc.rb
@@ -1,9 +1,9 @@
 class Gloc < Formula
   desc "Run a shell command in all the git repos in a directory"
   homepage "https://github.com/meain/gloc"
-  url "https://github.com/meain/gloc/releases/download/0.0.8/gloc_0.0.8_Darwin_x86_64.tar.gz"
-  version "0.0.8"
-  sha256 "ab29f10e74afaca05a5f4c1da20a94699c8396c39f3a76b4fecaadf7e8b3b9f0"
+  url "https://github.com/meain/gloc/releases/download/0.0.9/gloc_0.0.9_darwin_amd64.tar.gz"
+  version "0.0.9"
+  sha256 "9cfe1882f867a055d0ff0889f5d353e5a99972c92fa64277907391eae9e9cf85"
 
   def install
     bin.install "gloc"
