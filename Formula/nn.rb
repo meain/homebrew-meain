a4caec55bc089ffffc4669f4dc60fabbd4689031
meain <meain@users.noreply.github.com>
2022-01-26 03:20:01 +0000

    update brews

--- Formula/nn.rb
+++ Formula/nn.rb
@@ -1,9 +1,9 @@
 class Nn < Formula
   desc "Quick little bot to run shell commands on servers via matrix"
   homepage "https://github.com/meain/nn"
-  url "https://github.com/meain/nn/releases/download/0.0.2/nn_0.0.2_darwin_amd64.tar.gz"
-  version "0.0.2"
-  sha256 "f23ea87f1e6851da0b6380700eb9c63e06a7bf8cf49bcd48ff7fcce8185870e2"
+  url "https://github.com/meain/nn/releases/download/0.0.3/nn_0.0.3_darwin_amd64.tar.gz"
+  version "0.0.3"
+  sha256 "44413e3ae7ea9006ef13f3ba9fc6aa76cf26f125ea25f97dc96bfae819b7695e"
 
   def install
     bin.install "nn"
