20d6088bea143156d4485e9871e8c3d1e38c2f92
meain <meain@users.noreply.github.com>
2022-02-21 03:06:31 +0000

    update brews

--- Formula/sirus.rb
+++ Formula/sirus.rb
@@ -1,9 +1,9 @@
 class Sirus < Formula
   desc "Simple url shortner"
   homepage "https://github.com/meain/sirus"
-  url "https://github.com/meain/sirus/releases/download/0.0.7/sirus_0.0.7_darwin_amd64.tar.gz"
-  version "0.0.7"
-  sha256 "4e5d62cbb5f315a9689a2e964a02a151f40240e1ef404034d7c863878bcdb05f"
+  url "https://github.com/meain/sirus/releases/download/0.0.8/sirus_0.0.8_darwin_amd64.tar.gz"
+  version "0.0.8"
+  sha256 "9cbae1bb420b3859810c5943fd705d7ec45208d70c292c31eb8293bd185fc004"
 
   def install
     bin.install "gh-issues-to-rss"
