750e474e098e1e1c4b30f153ab86b077e0e4d27d
meain <meain@users.noreply.github.com>
2022-03-15 03:06:39 +0000

    update brews

--- Formula/toffee.rb
+++ Formula/toffee.rb
@@ -1,9 +1,9 @@
 class Toffee < Formula
   desc "Universal test picker"
   homepage "https://github.com/meain/toffee"
-  url "https://github.com/meain/toffee/releases/download/0.4.0/toffee-macos-amd64"
-  version "0.4.0"
-  sha256 "eda4e428878e08738cb84b470a0ec8308d8f0db85d8b1f3094666052dc60e6f0"
+  url "https://github.com/meain/toffee/releases/download/0.4.2/toffee-macos-amd64"
+  version "0.4.2"
+  sha256 "caed8532b6747ecd195fc25cd59a8e254dd5693c294fd5bebf684a3a9895ca03"
 
   def install
     mv Dir.glob("toffee-*").first, "toffee"
